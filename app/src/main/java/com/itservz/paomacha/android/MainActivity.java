461f25404b9e39800650d7e924b8d7ce57bb9cd0
rajuathokpam <raju.athokpam@gmail.com>
2016-12-31 00:52:34 +0530

    fullscreen

--- app/src/main/java/com/itservz/paomacha/android/MainActivity.java
+++ app/src/main/java/com/itservz/paomacha/android/MainActivity.java
@@ -1,7 +1,7 @@
 package com.itservz.paomacha.android;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,12 +11,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.Toast;
 
 import com.itservz.paomacha.android.event.EventBus;
 import com.itservz.paomacha.android.event.PageChangedEvent;
@@ -24,7 +23,7 @@ import com.itservz.paomacha.android.fragment.CentralCompositeFragment;
 import com.itservz.paomacha.android.view.VerticalPager;
 import com.squareup.otto.Subscribe;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     static final String TAG = "MainActivity";
     /**
@@ -33,22 +32,105 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private static final int CENTRAL_PAGE_INDEX = 1;
     private VerticalPager mVerticalPager;
 
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    //private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            Log.d(TAG, "mHidePart2Runnable");
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mVerticalPager.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "mShowPart2Runnable");
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "mHideRunnable");
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            Log.d(TAG, "mDelayHideTouchListener");
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, FullscreenActivity.class);
-                startActivity(i);
-                // close this activity
-                finish();
-            }
-        }, 500);
+        mVisible = true;
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        //mContentView = findViewById(R.id.activity_main_vertical_pager);
+        mVerticalPager = (VerticalPager) findViewById(R.id.activity_main_vertical_pager);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarTop);
+        // Set up the user interaction to manually show or hide the system UI.
+        mVerticalPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "mVerticalPager.setOnClickListener");
+                toggle();
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarTop);
         setSupportActionBar(toolbar);
 
         toolbar.setTitle(getTitle());
@@ -62,17 +144,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             //actionBar.setDisplayShowHomeEnabled(true);
-        }
-
-        final View decorView = getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener(
-                new View.OnSystemUiVisibilityChangeListener() {
-                    @Override
-                    public void onSystemUiVisibilityChange(int flags) {
-                        boolean visible = (flags & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0;
-                        Toast.makeText(MainActivity.this, "onSystemUiVisibilityChange", Toast.LENGTH_SHORT).show();
-                    }
-                });
+        }*/
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -86,24 +158,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         findViews();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        Toast.makeText(MainActivity.this, "onWindowFocusChanged", Toast.LENGTH_SHORT).show();
-        if (hasFocus) {
-            Toast.makeText(MainActivity.this, "onWindowFocusChanged hasFocus", Toast.LENGTH_SHORT).show();
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
-
     private void findViews() {
-        mVerticalPager = (VerticalPager) findViewById(R.id.activity_main_vertical_pager);
+
         initViews();
     }
 
@@ -112,8 +168,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void snapPageWhenLayoutIsReady(final View pageView, final int page) {
-		/*
-		 * VerticalPager is not fully initialized at the moment, so we want to snap to the central page only when it
+        /*
+         * VerticalPager is not fully initialized at the moment, so we want to snap to the central page only when it
 		 * layout and measure all its pages.
 		 */
         pageView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
@@ -137,6 +193,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         EventBus.getInstance().register(this);
@@ -147,6 +213,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         EventBus.getInstance().unregister(this);
         super.onPause();
     }
+
 
     @Subscribe
     public void onLocationChanged(PageChangedEvent event) {
@@ -164,26 +231,49 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return false;
     }
 
-    // This snippet hides the system bars.
-    private void hideSystemUI() {
-        // Set the IMMERSIVE flag.
-        // Set the content to appear under the system bars so that the content
-        // doesn't resize when the system bars hide and show.
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
     }
 
-    // This snippet shows the system bars. It does this by removing all the flags
-// except for the ones that make the content appear under the system bars.
-    private void showSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    private void hide() {
+        // Hide UI first
+        Log.d(TAG, "hide");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        Log.d(TAG, "show");
+        // Show the system bar
+        mVerticalPager.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        Log.d(TAG, "delayedHide");
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
--- app/src/main/java/com/itservz/paomacha/android/adapter/FragmentsClassesPagerAdapter.java
+++ app/src/main/java/com/itservz/paomacha/android/adapter/FragmentsClassesPagerAdapter.java
@@ -1,12 +1,16 @@
 package com.itservz.paomacha.android.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+
+import com.itservz.paomacha.android.fragment.CentralFragment;
+import com.itservz.paomacha.android.fragment.LeftFragment;
+import com.itservz.paomacha.android.fragment.RightFragment;
+
+import java.util.List;
 
 /**
  * Adapter for {@link ViewPager} that will populated from the collection of Fragments classes. Objects of that classes
@@ -26,8 +30,16 @@ public class FragmentsClassesPagerAdapter extends FragmentPagerAdapter {
 
 	@Override
 	public Fragment getItem(int position) {
-		return Fragment.instantiate(mContext, mPagesClasses.get(position).getName());
-	}
+        if (position == 0) {
+            return new LeftFragment();
+        } else if (position == 1) {
+            return new CentralFragment();
+        } else if (position == 2) {
+            return new RightFragment();
+        }
+        return null;
+        //return Fragment.instantiate(mContext, mPagesClasses.get(position).getName());
+    }
 
 	@Override
 	public int getCount() {
